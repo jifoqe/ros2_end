6807fb839992375bab43e0638bfcf0f237ceb379
jerry <10914132@gs.ltivs.ilc.edu.tw>
2026-01-03 02:29:27 +0800

    雲端測試14.0

--- rosbridge_ws/back/test_db.js
+++ rosbridge_ws/back/test_db.js
@@ -152,7 +152,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
