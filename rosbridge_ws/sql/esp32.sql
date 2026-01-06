7ecc38b31ebf1bc60b2d37e338af7d595bb4aeb2
n0967036096@gmail.com <n0967036096@gmail.com>
2026-01-06 11:38:32 +0800

    good

--- rosbridge_ws/sql/esp32.sql
+++ rosbridge_ws/sql/esp32.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-USE `zeabur`;
+USE `esp32`;
 
 --
 -- Database: `zeabur` (formerly esp32)
