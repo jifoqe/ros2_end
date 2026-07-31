729b59e77bfe5465bd72793aeedb5769e528378d
jerry <10914132@gs.ltivs.ilc.edu.tw>
2026-07-31 12:17:22 +0800

    最新的群組演算法

--- esp32_receiver/src/micro_ros_receiver.cpp
+++ esp32_receiver/src/micro_ros_receiver.cpp
@@ -1,6 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <nlohmann/json.hpp>
+// #include "rclcpp/qos.hpp"
+#include <algorithm>
 
 using json = nlohmann::json;
 using std::placeholders::_1;
@@ -10,15 +12,19 @@ class ESP32Subscriber : public rclcpp::Node
 public:
     ESP32Subscriber() : Node("micro_ros_receiver")
     {
+        // auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
+        // qos.reliable();
+
         // ===== Subscriber =====
         esp32_subscription_3 = this->create_subscription<std_msgs::msg::String>(
-            "esp32_topic_4",
+            "esp32_topic",
             10,
             std::bind(&ESP32Subscriber::topic_esp32_data_3, this, _1)
         );
 
         // ===== Publisher =====
-        move_pub_ = this->create_publisher<std_msgs::msg::String>("/move_data_4", 10);
+        move_pub_ = this->create_publisher<std_msgs::msg::String>("/move_data", 10);
+        // move_pub_ = this->create_publisher<std_msgs::msg::String>("/move_data",qos);
         web_output_pub_ = this->create_publisher<std_msgs::msg::String>("/web_output", 10);
     }
 
@@ -26,12 +32,61 @@ private:
     // ===== ESP32 callback =====
     void topic_esp32_data_3(const std_msgs::msg::String::SharedPtr msg)
     {
-        // int index = 3;
-        RCLCPP_INFO(this->get_logger(), "Received from ESP32_4: '%s'", msg->data.c_str());
-        // state[index] = std::stoi(msg->data);
-        // RCLCPP_INFO(this->get_logger(), "獲得資料: '%d'", state[index]);
-        // publish_web_output(msg->data);
-        // total_move_check(); //都移動好了一起動
+        char command[64];
+        int index = 0;
+        int number = 0;
+        RCLCPP_INFO(this->get_logger(), "ESP32: '%s'", msg->data.c_str());
+        
+
+
+        sscanf(msg->data.c_str(), "%d,%d,%s", &index, &number, command);
+        RCLCPP_INFO(this->get_logger(),"index=%d,number=%d, command=%s",index, number, command);
+
+        if(index == 1 && strcmp(command, "base_move_ok") == 0){
+            state[index-1] = number;
+        }else if(index == 2 && strcmp(command, "base_move_ok") == 0){
+            state[index-1] = number;
+        }else if(index == 3 && strcmp(command, "base_move_ok") == 0){
+            state[index-1] = number;
+        }else if(index == 4 && strcmp(command, "base_move_ok") == 0){
+            state[index-1] = number;
+        }
+
+        min_number = std::min({state[2], state[3]});
+
+        if(number>min_number){
+            return;
+        }
+
+        if(index == 1 && strcmp(command, "end") == 0){
+            state[index-1] = 0;
+        }else if(index == 2 && strcmp(command, "end") == 0){
+            state[index-1] = 0;
+        }else if(index == 3 && strcmp(command, "end") == 0){
+            state[index-1] = 0;
+        }else if(index == 4 && strcmp(command, "end") == 0){
+            state[index-1] = 0;
+        }
+
+        if(state[2] == state[3] && state[2] != 0 && state[3] != 0){
+            std_msgs::msg::String good;
+            good.data = std::to_string(min_number);
+            
+            RCLCPP_INFO(this->get_logger(),"同步移動");
+            for (int i = 0; i < 100; i++){
+                move_pub_->publish(good);
+                rclcpp::sleep_for(std::chrono::milliseconds(20)); // 間隔20ms
+            }
+        }else if(strcmp(command, "base_move_ok") == 0){
+            std_msgs::msg::String good;
+            good.data = std::to_string(min_number);
+            
+            RCLCPP_INFO(this->get_logger(),"最慢的要跟上");
+            for (int i = 0; i < 100; i++){
+                move_pub_->publish(good);
+                rclcpp::sleep_for(std::chrono::milliseconds(20)); // 間隔20ms
+            }
+        }
     }
 
     // ===== publish function =====
@@ -61,6 +116,7 @@ private:
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr move_pub_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr web_output_pub_;
     int state[4] = {0}; //0未完成, 1完成
+    int min_number = 0;
 };
 
 int main(int argc, char * argv[])
